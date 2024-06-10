63699476dc9846f4baa48ed8ae798fb3f33e51ba
swapnilDCT123 <swapnil.bankar@dctinc.com>
2024-06-10 16:18:28 +0530

    create and save user data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,4 +4,11 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+}
